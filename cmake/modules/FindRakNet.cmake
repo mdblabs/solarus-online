094e6f0a0dd645015758e4d906dbfb325c4acc25
mdb <mdb@mdblabs.com>
2018-03-29 22:04:00 +0200

    RakNet new approach 6

--- cmake/modules/FindRakNet.cmake
+++ cmake/modules/FindRakNet.cmake
@@ -1,22 +1,27 @@
 # Raknet
-MESSAGE("Buscamos Rakneti...")
+MESSAGE("Buscamos Raknet...")
 
 if(UNIX AND NOT APPLE)
 	SET(LINUX TRUE)
 endif()
+
+MESSAGE("LINUX--->" ${LINUX})
 
 if(LINUX)
 	find_library(RAKNET
 		NAMES RakNetStaticLib
 		HINTS "${CMAKE_SOURCE_DIR}/libraries/linux/"
 		)
+	MESSAGE("RAKNET--->" ${RAKNET})
 else()
 	find_library(RAKNET
 		NAMES RakNetStaticLib
 		HINTS "${CMAKE_SOURCE_DIR}/libraries/macos/"
 		)
+	MESSAGE("RAKNET--->" ${RAKNET})
 endif()
 
+MESSAGE("RAKNET_FOUND--->" ${RAKNET_FOUND})
 if(RAKNET_FOUND)
 	set(RAKNET_LIBRARY ${RAKNET})
 else(RAKNET_FOUND)
