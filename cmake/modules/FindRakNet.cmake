c37ce1a425da0a882208aa32dbad86d82948c162
mdb <mdb@mdblabs.com>
2018-03-29 21:37:42 +0200

    RakNet new approach 3

--- cmake/modules/FindRakNet.cmake
+++ cmake/modules/FindRakNet.cmake
@@ -1,22 +1,23 @@
-# - Find RakNet
-# Find the native RakNet includes and libraries
-#
-#  RAKNET_INCLUDE_DIR - where to find raknet headers, etc.
-#  RAKNET_LIBRARIES   - List of libraries when using libRakNetLibStatic.
-#  RAKNET_FOUND       - True if RakNet found.
+# Raknet
+if(UNIX AND NOT APPLE)
+	SET(LINUX TRUE)
+endif()
 
-find_library(RAKNET_LIBRARY NAMES RakNetLibStatic)
-
-# Handle the QUIETLY and REQUIRED arguments and set MODPLUG_FOUND to TRUE if
-# all listed variables are TRUE.
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(RAKNET DEFAULT_MSG
-    RAKNET_LIBRARY)
+if(LINUX)
+	find_library(RAKNET
+		NAMES RakNetStaticLib
+		HINTS "${CMAKE_SOURCE_DIR}/libraries/linux/"
+		)
+else()
+	find_library(RAKNET
+		NAMES RakNetStaticLib
+		HINTS "${CMAKE_SOURCE_DIR}/libraries/macos/"
+		)
+endif()
 
 if(RAKNET_FOUND)
-  set(RAKNET_LIBRARIES ${RAKNET_LIBRARY})
+	set(RAKNET_LIBRARY ${RAKNET})
 else(RAKNET_FOUND)
-  set(RAKNET_LIBRARIES)
+	set(RAKNET_LIBRARY)
 endif(RAKNET_FOUND)
 
-mark_as_advanced(RAKNET_LIBRARY)
