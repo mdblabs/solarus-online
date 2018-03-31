52ed7413e0528955d9ed28d62dfc9e0e8c6e8f63
mdb <mdb@mdblabs.com>
2018-03-31 13:35:53 +0200

    Return to original FindRakNet

--- cmake/modules/FindRakNet.cmake
+++ cmake/modules/FindRakNet.cmake
@@ -1,31 +1,22 @@
-# Raknet
-MESSAGE("Buscamos Raknet...")
+# - Find RakNet
+# Find the native RakNet includes and libraries
+#
+#  RAKNET_INCLUDE_DIR - where to find raknet headers, etc.
+#  RAKNET_LIBRARIES   - List of libraries when using libRakNetLibStatic.
+#  RAKNET_FOUND       - True if RakNet found.
 
-if(UNIX AND NOT APPLE)
-	SET(LINUX TRUE)
-endif()
+find_library(RAKNET_LIBRARY NAMES RakNetLibStatic)
 
-MESSAGE("LINUX--->" ${LINUX})
-MESSAGE("CMAKE SURCE--->" ${CMAKE_SOURCE_DIR})
+# Handle the QUIETLY and REQUIRED arguments and set MODPLUG_FOUND to TRUE if
+# all listed variables are TRUE.
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(RAKNET DEFAULT_MSG
+    RAKNET_LIBRARY)
 
-if(LINUX)
-	find_library(RAKNET
-		NAMES RakNetStaticLib
-		HINTS "${CMAKE_SOURCE_DIR}/libraries/linux/"
-		)
-	MESSAGE("RAKNET--->" ${RAKNET})
-else()
-	find_library(RAKNET
-		NAMES RakNetStaticLib
-		HINTS "${CMAKE_SOURCE_DIR}/libraries/macos/"
-		)
-	MESSAGE("RAKNET--->" ${RAKNET})
-endif()
-
-MESSAGE("RAKNET_FOUND--->" ${RAKNET_FOUND})
 if(RAKNET_FOUND)
-	set(RAKNET_LIBRARY ${RAKNET})
+  set(RAKNET_LIBRARIES ${RAKNET_LIBRARY})
 else(RAKNET_FOUND)
-	set(RAKNET_LIBRARY)
+  set(RAKNET_LIBRARIES)
 endif(RAKNET_FOUND)
 
+mark_as_advanced(RAKNET_LIBRARY)
