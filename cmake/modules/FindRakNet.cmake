85245486976d3c7a65804af2c9f4ac39f1838871
mdb <mdb@mdblabs.com>
2018-03-31 19:00:17 +0200

    RakNet linking OK macos

--- cmake/modules/FindRakNet.cmake
+++ cmake/modules/FindRakNet.cmake
@@ -5,7 +5,16 @@
 #  RAKNET_LIBRARIES   - List of libraries when using libRakNetLibStatic.
 #  RAKNET_FOUND       - True if RakNet found.
 
-find_library(RAKNET_LIBRARY NAMES RakNetLibStatic)
+# find_library(RAKNET_LIBRARY 
+#	NAMES RakNetLibStatic
+#	HINTS ${CMAKE_SOURCE}/libraries/macos/
+#	)
+
+if(UNIX AND APPLE)
+	set(RAKNET_LIBRARY ${CMAKE_SOURCE_DIR}/libraries/macosx/libRakNetLibStatic.a)
+else()
+	set(RAKNET_LIBRARY ${CMAKE_SOURCE_DIR}/libraries/linux/libRakNetLibStatic.a)
+endif()
 
 # Handle the QUIETLY and REQUIRED arguments and set MODPLUG_FOUND to TRUE if
 # all listed variables are TRUE.
